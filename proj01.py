140f70dcbe7afa19c2e9f070558f15608782281c
Ritesh Dhakal <37334512+rdhakal098@users.noreply.github.com>
2021-03-09 20:03:54 -0500

    Finished

---
+++ proj01.py
@@ -0,0 +1,7 @@
+print("How many Richters are you converting")
+richter = float(input())
+print("Richter scale Value:", richter)
+joules = 10**((1.5*richter)+4.8)
+print("Amount of Joules:", joules)
+tnt = joules / (4.184*10**9)
+print("Amount of TNT:", tnt)
